0a9377020a50b3a9d64d48c15559438dbb2926c3
Hernan Santarelli <hernansantarelli87@gmail.com>
2022-12-27 22:07:39 -0300

    I created the functionality to add products to stock

--- src/components/AdminForm.jsx
+++ src/components/AdminForm.jsx
@@ -1,6 +1,28 @@
+import { uploadImage, db } from "../firebase/firebase";
+import { collection, addDoc } from "firebase/firestore"; 
+
+
 export default function AdminForm() {
+  
+  const stockRef = collection(db, 'stock')
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const image = await uploadImage(e.target.image.files[0])
+    console.log('esta es la constante', image)
+    
+    addDoc(stockRef, {
+      name: e.target.name.value,
+      description: e.target.description.value,
+      price: e.target.price.value,
+      img: image
+    })
+    
+    e.target.image.value = null;
+  }
+
   return (
-    <form className="p-2 flex flex-col h-screen bg-slate-400">
+    <form className="p-2 flex flex-col h-screen bg-slate-400" onSubmit={handleSubmit}>
       <label htmlFor="name">Nombre del producto</label>
       <input
         type="text"
@@ -9,7 +31,7 @@ export default function AdminForm() {
         className="border-2 my-2 "
       />
       <label htmlFor="price">Precio</label>
-      <input type="number" placeholder="$ARS" className="border-2 my-2" />
+      <input type="number" id="price" placeholder="$ARS" className="border-2 my-2" />
       <label htmlFor="description">Descripcion</label>
       <textarea
         name=""
@@ -18,8 +40,8 @@ export default function AdminForm() {
         rows="3"
         className="border-2 my-2"
       ></textarea>
-      <label htmlFor="images">Imagen</label>
-      <input type="file" className="my-2" />
+      <label htmlFor="image">Imagen</label>
+      <input type="file" className="my-2" id="image" />
       <button type="submit" className="w-fit my-2 px-2 py-1 text-white bg-blue-600">Guardar</button>
     </form>
   );
